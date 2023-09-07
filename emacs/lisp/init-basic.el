74b5a91ccabee0eb3a81856b91ae9c4afca78205
simileci.wh <simileci.wh.32@outlook.com>
2023-09-07 21:18:46 +0800

    <EMACS><FIX> fix line number high light color. @2023/09/07:(21:18)
    Signed-off-by: simileci.wh <simileci.wh.32@outlook.com>

--- emacs/lisp/init-basic.el
+++ emacs/lisp/init-basic.el
@@ -95,7 +95,7 @@
 (require 'crosshairs)
 (crosshairs-mode t)
 (setq toggle-crosshairs-when-idle 0.1)
-(set-face-background 'hl-line "#696969")
+(set-face-background 'hl-line "#104e8b")
 (set-face-attribute 'col-highlight nil :inherit 'hl-line :background)
 
 
