b1fa7d1cc611ae0a46f9c886ac3b3ffc30fbc785
h_wang <h_wang@raymx-micro.com>
2023-09-11 21:24:34 +0800

    <EMACS><FEATURE> org-download feature enable for win. <2023-09-11 1 21:24>
    Signed-off-by: h_wang <h_wang@raymx-micro.com>

--- emacs/lisp/init-org.el
+++ emacs/lisp/init-org.el
@@ -1,4 +1,6 @@
 
+(setq org-return-follows-link t)
+
 (use-package org
   :ensure nil
   :init
@@ -8,6 +10,11 @@
 	org-log-done 'time
 	;; Tags setting
 	org-use-tag-inheritance nil
+	;; property
+	;; org-use-property-inheritance
+	;;time stamps
+	org-deadline-warning-days 7
+	org-image-actual-width nil
 	)
   (setq org-todo-keywords
 	'((sequence "TODO(t)" "|" "DONE(d!)")
@@ -51,7 +58,35 @@
 
   (add-hook 'org-after-todo-statistics-hook #'org-summary-todo)
   )
-  
+
+(use-package org-download
+    :demand t
+    :after org
+    :hook (dired-mode . org-download-enable)
+    :config
+    (cond  (*is-mac*
+
+	    )
+	   (*is-windows*
+	    (setq org-download-screenshot-method "powershell -c Add-Type -AssemblyName System.Windows.Forms;$image = [Windows.Forms.Clipboard]::GetImage();$image.Save('%s', [System.Drawing.Imaging.ImageFormat]::Png)")
+
+	    )
+	   (t "need check for linux")
+	   
+	   )
+ 
+    (defun org-download-annotate-default (link)
+      "Annotate LINK with the time of download."
+      (make-string 0 ?\s))
+
+    (setq-default org-download-heading-lvl nil
+		  org-download-image-dir "./img"
+		  ;; org-download-screenshot-method "screencapture -i %s"
+		  org-download-screenshot-file (expand-file-name "screenshot.jpg" temporary-file-directory)
+		  )
+
+    )
+
 
 
 (provide 'init-org)
