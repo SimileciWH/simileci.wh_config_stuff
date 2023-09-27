4e5ca780662edfb67f266209cb0ac5ce4ec2fa4e
simileci.wh <simileci.wh.32@outlook.com>
2023-09-27 21:41:58 +0800

    <EMACS><MINOR> copy function. <2023-09-27 3 21:41>
    Signed-off-by: simileci.wh <simileci.wh.32@outlook.com>

--- emacs/lisp/init-func.el
+++ emacs/lisp/init-func.el
@@ -87,6 +87,25 @@ supports exporting consult-grep to wgrep, file to wdeired, and consult-location 
   (kill-line 1)
   )
 
+(defun my-makefile-indent-line ()
+  (save-excursion (forward-line 0)
+		  (cond
+		   ;; keep TABs
+		   ((looking-at "\t")
+		    t)
+		   ;; indent continuation lines to 4
+		   ((and (not (bobp))
+			 (= (char-before (1- (point))) ?\\))
+		    (delete-horizontal-space)
+		    (indent-to 4))
+		   ;; delete all other leading whitespace
+		   ((looking-at "\\s-+")
+		    (replace-match ""))))
+  )
+(add-hook 'makefile-mode-hook
+	  (lambda ()
+	    (setq-local indent-line-function 'my-makefile-indent-line))
+	  )
 
 
 (provide 'init-func)
