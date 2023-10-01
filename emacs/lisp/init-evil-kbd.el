9a3fbd1792fd887e12e8f77f8b63a7f6eaf8f1e9
h_wang <h_wang@raymx-micro.com>
2023-10-01 12:56:21 +0800

    <EMACS><EVIL> enable plugins evil-anzu evil-collection evil-surround evil-snip evil-matchit. <2023-10-01 0 12:56>
    Signed-off-by: h_wang <h_wang@raymx-micro.com>

---
+++ emacs/lisp/init-evil-kbd.el
@@ -0,0 +1,18 @@
+(define-key evil-insert-state-map [escape] 'evil-normal-state)
+(define-key evil-normal-state-map (kbd "[ SPC") (lambda () (interactive) (evil-insert-newline-above) (forward-line)))
+(define-key evil-normal-state-map (kbd "] SPC") (lambda () (interactive) (evil-insert-newline-below) (forward-line -1)))
+
+(define-key evil-normal-state-map (kbd "[ b") 'previous-buffer)
+(define-key evil-normal-state-map (kbd "] b") 'next-buffer)
+(define-key evil-motion-state-map (kbd "[ b") 'previous-buffer)
+(define-key evil-motion-state-map (kbd "] b") 'next-buffer)
+
+(define-key evil-normal-state-map (kbd ",/") 'evilnc-comment-or-uncomment-lines)
+(define-key evil-visual-state-map (kbd ",/") 'evilnc-comment-or-uncomment-lines)
+
+(evil-define-key 'normal dired-mode-map
+  (kbd "<RET>") 'dired-find-alternate-file
+  (kbd "C-k") 'dired-up-directory
+  )
+
+(provide 'init-evil-kbd)
--- emacs/lisp/init-evil.el
+++ emacs/lisp/init-evil.el
@@ -1,9 +1,11 @@
+
 (use-package evil
   :init
   (setq evil-want-keybinding nil
 	evil-want-C-u-scroll t
 	)
   (evil-mode)
+  (setcdr evil-insert-state-map nil)
 
   ;; https://emacs.stackexchange.com/questions/46371/how-can-i-get-ret-to-follow-org-mode-links-when-using-evil-mode
   (with-eval-after-load 'evil-maps
@@ -11,18 +13,58 @@
   )
 (use-package undo-tree
   :diminish
-  :config
+  :init
   (setq
    undo-tree-mode t
    global-undo-tree-mode t
    global-undo-tree-mode-buffers t
    undo-tree-visualizer-diff t
    undo-tree-visualizer-timestamps t
+   undo-tree-auto-save-history nil
    )
   (evil-set-undo-system 'undo-tree)
   )
 
 (use-package evil-tutor-sc)
+(use-package evil-anzu
+  :after evil
+  :diminish
+  :demand t
+  :init
+  (global-anzu-mode t)
+  )
+(use-package evil-collection
+  :demand t
+  :config
+  (setq evil-collection-mode-list (remove 'lispy evil-collection-mode-list))
+  (evil-collection-init)
 
+  (cl-loop for (mode . state) in
+	       '((org-agenda-mode . normal)
+	         (Custom-mode . emacs)
+	         (eshell-mode . emacs)
+	         (makey-key-mode . motion))
+	       do (evil-set-initial-state mode state)
+           )
+  )
+(use-package evil-surround
+  :ensure t
+  :init
+  (global-evil-surround-mode 1)
+  )
+(use-package evil-nerd-commenter)
+
+(use-package evil-snipe
+  :diminish
+  :init
+  (evil-snipe-mode +1)
+  (evil-snipe-override-mode +1)
+  )
+(use-package evil-matchit
+  :ensure
+  :init
+  (global-evil-matchit-mode 1)
+  )
+(require 'init-evil-kbd)
 
 (provide 'init-evil)
