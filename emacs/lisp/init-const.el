099c8586446509e9f54e52991bb28d76fdd0b923
h_wang <simileci.wh.32@outlook.com>
2023-10-14 18:26:54 +0800

    <EMACS><IMPROVE> when save file delete white space EOL.
    Signed-off-by: h_wang <simileci.wh.32@outlook.com>

--- emacs/lisp/init-const.el
+++ emacs/lisp/init-const.el
@@ -2,17 +2,17 @@
 (defconst *is-linux* (eq system-type 'gnu/linux))
 (defconst *is-windows* (or (eq system-type 'ms-dos) (eq system-type 'windows-nt)))
 (cond  (*is-mac*
-	(defconst *WinMac-PATH* "/Volumes/SS980/SWStuff/OneDrive/WinMacSwapStuff")
-	(defconst *work-space-PATH* "/Volumes/SS980/work_space")
-	)
+    (defconst *WinMac-PATH* "/Volumes/SS980/SWStuff/OneDrive/WinMacSwapStuff")
+    (defconst *work-space-PATH* "/Volumes/SS980/work_space")
+    )
        (*is-windows*
-	(defconst *WinMac-PATH* "e:/NewE/OneDrive/WinMacSwapStuff")
-	(defconst *work-space-PATH* "d:/work_space")
-	)
+    (defconst *WinMac-PATH* "e:/NewE/OneDrive/WinMacSwapStuff")
+    (defconst *work-space-PATH* "d:/work_space")
+    )
        (t "need check for linux"
 
-	  )
-       
+      )
+
        )
 (defadvice align-regexp (around align-regexp-with-spaces activate)
   (let ((old-indent-tabs-mode indent-tabs-mode))
@@ -20,5 +20,11 @@
     ad-do-it
     (setq indent-tabs-mode old-indent-tabs-mode))
   )
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+;; (add-hook 'before-save-hook 'whitespace-cleanup)
+
+;; (add-hook 'before-save-hook 'delete-blank-lines)
+
 
 (provide 'init-const)
