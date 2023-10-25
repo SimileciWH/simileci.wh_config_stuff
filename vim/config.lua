6a0b4dba6251017ad2b6cd783347052b2085d06a
simileci.wh <simileci.wh.32@outlook.com>
2023-10-26 05:20:08 +0800

    <NVIM><LUANRVIM> config sync. <2023-10-26 4 05:20>
    Signed-off-by: simileci.wh <simileci.wh.32@outlook.com>

---
+++ vim/config.lua
@@ -0,0 +1,10 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.keys.normal_mode["<C-s>"] = ":w<CR>"
+lvim.keys.normal_mode["<C-x>"] = ":q!<CR>"
+lvim.keys.normal_mode["<[-b>"] = ":bprevious<CR>"
+lvim.keys.normal_mode["<]-b>"] = ":bnext<CR>"
+
